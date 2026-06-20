bbc880e7a5d5fa373da8b1d2f13602a70ec84993
bisket74-eng <bisket74@gmail.com>
2026-06-20 10:39:09 -0700

    Change trivia fetch difficulty from medium to easy

--- games.js
+++ games.js
@@ -1612,7 +1612,7 @@ window.handleSequenceCellTap = function (idx) {
     const fallback = LOCAL_TRIVIA.slice();
 
     try {
-        const res = await fetch("https://opentdb.com/api.php?amount=20&category=9&difficulty=medium&type=multiple");
+        const res = await fetch("https://opentdb.com/api.php?amount=20&category=9&difficulty=easy&type=multiple");
         const data = await res.json();
 
         if (!data.results || !data.results.length) throw new Error("No questions");
